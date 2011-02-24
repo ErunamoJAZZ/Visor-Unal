1cda261143105649fd4998457749c747cfc7de24
C. Daniel Sanchez R. ErunamoJAZZ <anonimato1990@gmail.com>
2011-02-24 11:02:50 -0500

    Inicializando proyecto.

---
+++ src/VisorMain.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+Created on 24/02/2011
+
+@author: Carlos Daniel Sanchez Ramirez <ErunamoJAZZ>
+@web: https://github.com/ErunamoJAZZ/Visor-Unal
+'''
+
+if __name__ == '__main__':
+    print (u"Hola Mundo!!")
+    print (u"Usaré OpenCV y wxPython para hacer este programa :)")
